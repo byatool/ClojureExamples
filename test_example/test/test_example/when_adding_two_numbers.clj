6f6b53c89157229c9414df3a9566b9e542a68466
unknown <ssparling@INFOTK133.mctwf.org>
2013-04-16 18:02:56 -0400

    no idea

--- test_example/test/test_example/when_adding_two_numbers.clj
+++ test_example/test/test_example/when_adding_two_numbers.clj
@@ -1,9 +1,48 @@
+(defmacro when [text & body]
+  (let [namespace (create-symbol-from-string text)]
+    `(~(create-symbol-from-string "ns") ~namespace
+      (:use [clojure.test :as test])
+      ~@body)))
+
+(defmacro it-should [text & body]
+  `(test/deftest ~(create-symbol-from-string text)
+     (test/testing ~text
+       ~@body)))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 (ns test-example.when-adding-two-numbers
   (:use clojure.test
         test-example.simple-math))
 
 (deftest when-adding-two-numbers
-	(let [expected-number 2]
-		(testing "It will return the second if the first is zero"
-    	(is (= (add-two-numbers 0 expected-number) expected-number)))))
+  (let [expected-number 1]
+    (testing "It will return the second if the first is zero"
+      (is (= (add-two-numbers 1 expected-number) expected-number)))))
+
+(defn create-symbol-from-string [text]
+  (symbol (join "-" (split text #" "))))
+
+
+
+
+
+
+
+(when "something is here"
+  [(it-should "test stuff" (is (= 1 2)))
+   (it-should "test things" (is (= 2 3)))])
 
